9f7b1f2553d4f084ef22c44fca1dce508e9c2e25
Eren <vrsozluk@gmail.com>
2023-03-03 22:14:29 +0300

    displaying photo function has been added

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,8 @@ const App = () => {
     try {
       const response = await fetch(url);
       const data = await response.json();
+      setPhotos(data);
+      setLoading(false);
     } catch (error) {
       setLoading(false);
       console.log(error);
@@ -26,7 +28,30 @@ const App = () => {
   useEffect(() => {
     fetchImages();
   }, []);
-  return <h2>stock photos starter</h2>;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <main>
+      <section className="search">
+        <form className="search-form">
+          <input type="text" placeholder="search" className="form-input" />
+          <button className="submit-btn" type="submit" onClick={handleSubmit}>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((image, index) => {
+            return <Photo key={image.id} {...image} />;
+          })}
+        </div>
+        {loading && <h2 className="loading">Loading...</h2>}
+      </section>
+    </main>
+  );
 };
 
 export default App;
